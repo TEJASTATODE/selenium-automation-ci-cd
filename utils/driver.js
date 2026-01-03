26eb9079a27f6314d4fb0221eafcf2bd94b69b42
Tejas Tatode <146309839+TEJASTATODE@users.noreply.github.com>
2026-01-03 18:44:21 +0530

    Update driver.js

--- utils/driver.js
+++ utils/driver.js
@@ -1,17 +1,29 @@
 const { Builder } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
+const { ServiceBuilder } = require("selenium-webdriver/chrome");
 require("chromedriver");
 
 async function getDriver() {
   const options = new chrome.Options();
-  options.addArguments("--headless");
-  options.addArguments("--no-sandbox");
-  options.addArguments("--disable-dev-shm-usage");
+
+  options.addArguments(
+    "--headless",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-gpu"
+  );
+
+  options.setChromeBinaryPath("/usr/bin/chromium");
+
+  
+  const service = new ServiceBuilder("/usr/bin/chromedriver");
 
   return new Builder()
     .forBrowser("chrome")
     .setChromeOptions(options)
+    .setChromeService(service)
     .build();
 }
 
 module.exports = getDriver;
+
